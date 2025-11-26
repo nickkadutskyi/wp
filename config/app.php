d48eb8a7db6bc5785c360c778ec75a7976f4bd79
Nick Kadutskyi <nick@kadutskyi.com>
2025-11-26 11:39:23 -0800

    env vars filtering

--- config/app.php
+++ config/app.php
@@ -66,7 +66,7 @@ if ( ! empty( $missing_env_vars ) ) {
 // Sets WP's environment (local, development, staging, production).
 define( 'APP_ENV', $_ENV['APP_ENV'] ?? 'production' );
 if ( ! ( $_ENV['WP_ENVIRONMENT_TYPE'] ?? null ) && in_array( APP_ENV, [ 'production', 'staging', 'development', 'local' ], true ) ) {
-    define( 'WP_ENVIRONMENT_TYPE', $_ENV['WP_ENVIRONMENT_TYPE'] ?? 'development' );
+    define( 'WP_ENVIRONMENT_TYPE', $_ENV['WP_ENVIRONMENT_TYPE'] ?? 'production' );
 }
 
 
@@ -121,18 +121,18 @@ if ( isset( $_ENV['DATABASE_URL'] ) ) {
 
 // Settings.
 // Prevent plugin and theme modifications via file editor.
-define( 'DISALLOW_FILE_EDIT', $_ENV['DISALLOW_FILE_EDIT'] ?? true );
+define( 'DISALLOW_FILE_EDIT', filter_var( $_ENV['DISALLOW_FILE_EDIT'] ?? true, FILTER_VALIDATE_BOOL ) );
 // Prevent all plugin and theme updates and installation.
-define( 'DISALLOW_FILE_MODS', $_ENV['DISALLOW_FILE_MODS'] ?? true );
-define( 'AUTOMATIC_UPDATER_DISABLED', $_ENV['AUTOMATIC_UPDATER_DISABLED'] ?? true );
-define( 'WP_AUTO_UPDATE_CORE', $_ENV['WP_AUTO_UPDATE_CORE'] ?? false );
+define( 'DISALLOW_FILE_MODS', filter_var( $_ENV['DISALLOW_FILE_MODS'] ?? true, FILTER_VALIDATE_BOOL ) );
+define( 'AUTOMATIC_UPDATER_DISABLED', filter_var( $_ENV['AUTOMATIC_UPDATER_DISABLED'] ?? true, FILTER_VALIDATE_BOOL ) );
+define( 'WP_AUTO_UPDATE_CORE', filter_var( $_ENV['WP_AUTO_UPDATE_CORE'] ?? false, FILTER_VALIDATE_BOOL ) );
 // Disable concatenation of scripts.
-define( 'CONCATENATE_SCRIPTS', $_ENV['CONCATENATE_SCRIPTS'] ?? false );
+define( 'CONCATENATE_SCRIPTS', filter_var( $_ENV['CONCATENATE_SCRIPTS'] ?? false, FILTER_VALIDATE_BOOL ) );
 
 // Limit the number of post revisions.
-define( 'WP_POST_REVISIONS', $_ENV['WP_POST_REVISIONS'] ?? true );
+define( 'WP_POST_REVISIONS', filter_var( $_ENV['WP_POST_REVISIONS'] ?? true, FILTER_VALIDATE_BOOL ) );
 
-define( 'DISABLE_WP_CRON', $_ENV['DISABLE_WP_CRON'] ?? false );
+define( 'DISABLE_WP_CRON', filter_var( $_ENV['DISABLE_WP_CRON'] ?? false, FILTER_VALIDATE_BOOL ) );
 
 /**
  * Allow WordPress to detect HTTPS when used behind a reverse proxy or load balancer
@@ -148,10 +148,12 @@ if ( 'https' === ( $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '' )
 /**
  * Debugging Settings
  */
-define( 'WP_DEBUG', $_ENV['WP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false );
-define( 'WP_DEBUG_DISPLAY', $_ENV['WP_DEBUG_DISPLAY'] ?? $_ENV['APP_DEBUG'] ?? false );
-define( 'WP_DEBUG_LOG', $_ENV['WP_DEBUG_LOG'] ?? $_ENV['APP_DEBUG'] ?? false );
-define( 'SCRIPT_DEBUG', $_ENV['SCRIPT_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false );
+define( 'SAVEQUERIES', filter_var( $_ENV['SAVEQUERIES'] ?? $_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL ) );
+define( 'WP_DEBUG', filter_var( $_ENV['WP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL ) );
+define( 'WP_DEBUG_DISPLAY', filter_var( $_ENV['WP_DEBUG_DISPLAY'] ?? $_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL ) );
+define( 'WP_DEBUG_LOG', filter_var( $_ENV['WP_DEBUG_LOG'] ?? $_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL ) );
+define( 'SCRIPT_DEBUG', filter_var( $_ENV['SCRIPT_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL ) );
+define( 'WP_DISABLE_FATAL_ERROR_HANDLER', filter_var( $_ENV['WP_DISABLE_FATAL_ERROR_HANDLER'] ?? $_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL ) );
 
 /**
  * Authentication unique keys and salts.
